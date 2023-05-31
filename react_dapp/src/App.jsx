2288fcd9273bcceb6764bfb3d2bd785cd957725d
Mahtelly25 <fnjuguna706@gmail.com>
2023-05-31 18:16:56 +0300

    Fix css error

--- react_dapp/src/App.jsx
+++ react_dapp/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddCandidate from "./pages/AddCandidate";
 
 import Home from "./pages/Home";
-import "./app.css";
+import "./App.css";
 import Candidates from "./pages/Candidates";
 const App = () => {
   return (
